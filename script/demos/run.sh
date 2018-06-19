f14f4d1089be7f7dc96d11a807e5fd9163c9dec6
rohanpsingh <rohan565singh@gmail.com>
2018-06-19 16:31:36 +0900

    run in xterm window

--- script/demos/run.sh
+++ script/demos/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash         
 
-gepetto-viewer-server & 
-hpp-rbprm-server &
+xterm -hold -e gepetto-viewer-server & 
+xterm -hold -e hpp-rbprm-server &
 ipython -i --no-confirm-exit ./$1
 
 pkill -f  'gepetto-viewer-server'
